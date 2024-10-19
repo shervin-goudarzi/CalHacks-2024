2bcdfa059eb9403b298908ee2887652cbb9a841c
PopsicleSnow <popsiclehungry@gmail.com>
2024-10-18 21:24:31 -0700

    added gooogle login

---
+++ CalHacks_2024/react_oauth_google.py
@@ -0,0 +1,17 @@
+import reflex as rx
+
+
+class GoogleOAuthProvider(rx.Component):
+    library = "@react-oauth/google"
+    tag = "GoogleOAuthProvider"
+
+    client_id: rx.Var[str]
+
+class GoogleLogin(rx.Component):
+    library = "@react-oauth/google"
+    tag = "GoogleLogin"
+
+    on_success: rx.EventHandler[lambda data: [data]]
+    on_failure: rx.EventHandler[lambda error: [error]]
+
+    
